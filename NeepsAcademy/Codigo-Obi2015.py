60369c0a46d363de0fc9788b2238f219c43856ca
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2023-07-01 22:19:04 -0300

    exercicio da gangorra e exercicio da obi de 2025 concluidos
    finalizados com sucesso :)

--- NeepsAcademy/Codigo-Obi2015.py
+++ NeepsAcademy/Codigo-Obi2015.py
@@ -2,6 +2,11 @@ def main():
     # Seu código vai aqui
     n = int(input(''))
     a = str(input(''))
+    temp = a.replace(' ', '')
+
+    sla = temp.count('100')
+    print(sla)
+
     
 
 if __name__ == "__main__":
