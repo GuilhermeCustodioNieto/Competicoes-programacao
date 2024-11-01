b458035e0c4c233a04397ac51dca5e88e2fcc10a
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2024-11-01 14:00:41 -0300

    Test

---
+++ Kaique-Palestra/coral.py
@@ -0,0 +1,8 @@
+c1, c2, c3, c4 = map(int, input().split())
+
+eCoral = False
+
+if c1 == c3 or c2 == c4:
+    print('V')
+else:
+    print('F')
---
+++ Kaique-Palestra/test.py
@@ -0,0 +1,6 @@
+numCaixa, maximo = map(int, input().split())
+
+contador = 0
+
+for i in range(3):
+    if 
